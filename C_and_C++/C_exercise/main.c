049d3db5002d1466892f05775689d9f8713412d1
stuian <892545949@qq.com>
2021-03-16 18:49:06 +0800

    210316 c

--- C_and_C++/C_exercise/main.c
+++ C_and_C++/C_exercise/main.c
@@ -1,23 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <stdio.h>
-
 int main()
 {
-        char *p1[5] = {
-                "让编程改变世界 -- 鱼C工作室",
-                "Just do it -- NIKE",
-                "一切皆有可能 -- 李宁",
-                "永不止步 -- 安踏",
-                "One more thing... -- 苹果"
-        };
-        int i;
+        int array[2][3] = {{1,2,3},{4,5,6}};
+        int (*p)[3] = array;
 
-        for (i = 0; i < 5; i++)
-        {
-                printf("%s\n", p1[i]);
-        }
+        // 同一个地址
+        printf("array:%p\n",array);
+        printf("array[0]:%p\n",array[0]);
+        printf("&array[0][0]:%p\n",&array[0][0]);
+        printf("\n");
+
+        //同一个地址
+        printf("*(array+1):%p\n",*(array+1));
+        printf("array[1]:%p\n",array[1]);
+        printf("&array[1][0]:%p\n",&array[1][0]);
+        printf("\n");
+
+        printf("*(*(array+1)+2):%p\n",*(*(array+1)+2));
+        printf("array[1][2]:%p\n",array[1][2]);
+        printf("array[1][2]:%d\n",array[1][2]);
+        printf("\n");
+
+        //指针操作数组
+        printf("**p:%d\n",**(p+1));
+        printf("**(array+1):%d\n",**(array+1));
+        printf("array[1][0]:%d\n",array[1][0]);
+
+        printf("*(*(p+1)+2): %d\n", *(*(p+1)+2));
+        printf("*(*(array+1)+2): %d\n", *(*(array+1)+2));
+        printf("array[1][2]: %d\n", array[1][2]);
 
         return 0;
 }
